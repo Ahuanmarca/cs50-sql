c4d4218b94ca696a8148c1edbb27effa2499eb9f
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-03-10 19:11:05 +0100

    Refactoring pset1 to contain sqlite and postgres examples!

--- pset1/dese/2.sql
+++
@@ -1,2 +0,0 @@
-SELECT name NAME from districts
-  WHERE name LIKE "%(non-op)";
--- pset1/dese/6.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-  schools.name
-FROM
-  schools
-  INNER JOIN graduation_rates ON graduation_rates.school_id = schools.id
-WHERE
-  graduation_rates.graduated = 100;
